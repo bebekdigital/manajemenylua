dab822cbbd2eb39ab821fa33663008dc60ab076e
Admin Digital <badanwakafliulilalbab@gmail.com>
2026-09-14 10:56:21 +0700

    ubah warna

--- resources/js/Layouts/AppLayout.vue
+++ resources/js/Layouts/AppLayout.vue
@@ -42,7 +42,7 @@ const activeRouteLabel = computed(() => {
 </script>
 
 <template>
-    <div class="bg-emerald-50 text-on-background font-body-md min-h-screen flex flex-col md:flex-row antialiased">
+    <div class="bg-gray-100 text-on-background font-body-md min-h-screen flex flex-col md:flex-row antialiased">
         <!-- Persistent Sidebar -->
         <SidebarNav :current-route="activeRoute" :is-open="isSidebarOpen" @close="closeSidebar" />
 
