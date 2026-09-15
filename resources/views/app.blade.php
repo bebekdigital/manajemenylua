a39997e7883da891112cb12a39acf5be9ca2d2ac
Nama Anda <email@anda.com>
2026-09-15 10:07:57 +0700

    ubah favicon

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,11 +3,12 @@
 <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Foundation Data Center - Yayasan Li Ulil Albab Karanganyar</title>
+    <title>Data Center - Yayasan Li Ulil Albab Karanganyar</title>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&family=Plus+Jakarta+Sans:wght@500;600;700;800&display=swap" rel="stylesheet" />
     <link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&display=swap" rel="stylesheet" />
+    <link rel="icon" type="image/png" href="{{ asset('favicon.png') }}">
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     @inertiaHead
 </head>
